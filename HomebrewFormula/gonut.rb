0eaa95d0044e96d3fa42fbaad05d1fcae0da44fc
Matthias Diester <matthias.diester@de.ibm.com>
2019-03-19 16:15:34 +0100

    Add initial gonut version
    Add gonut version v0.2.0-alpha as the initial version to be used by the
    homeport tap.

---
+++ HomebrewFormula/gonut.rb
@@ -0,0 +1,44 @@
+# MIT License
+#
+# Copyright (c) 2019 The Homeport Team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'FileUtils'
+
+# Just the gonut homebrew formula
+class Gonut < Formula
+  yver = "v0.2.0-alpha"
+  ysha = "4645a644eea53b1801cc6e2e8d280e8ec4aec0b8ed21fce700b0b334bea94786"
+
+  desc 'gonut - a portable tool to help you verify whether you can push a sample app to a Cloud Foundry'
+  homepage 'https://github.com/homeport/gonut'
+  url "https://github.com/homeport/gonut/releases/download/#{yver}/gonut-darwin-amd64"
+  sha256 "#{ysha}"
+  version "#{yver}"
+
+  def install
+    FileUtils.mv('gonut-darwin-amd64', 'gonut')
+    bin.install 'gonut'
+  end
+
+  test do
+    system "#{bin}/gonut", 'version'
+  end
+end
