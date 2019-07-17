7ce2ef031b6aa66d9f8c79a0c59995d82b7c6ab5
Tareq Al-Maamari <tariq.mamari@de.ibm.com>
2019-07-17 14:04:45 +0200

    Unified formula style and setup
    Comply to brew audit style (mostly).
    
    Signed-off-by: Matthias Diester <matthias.diester@de.ibm.com>

--- HomebrewFormula/dyff.rb
+++ HomebrewFormula/dyff.rb
@@ -1,10 +1,9 @@
-# Just the dyff homebrew formula
 class Dyff < Formula
   desc "δyƒƒ /ˈdʏf/ - A diff tool for YAML files, and sometimes JSON"
   homepage "https://github.com/homeport/dyff"
   url "https://github.com/homeport/dyff/releases/download/v0.10.2/dyff-darwin-amd64"
-  sha256 "9b5979051f8cd3757104a7cdae0a1eafc5af11dc4a87830c62b02cbb8c82019a"
   version "v0.10.2"
+  sha256 "9b5979051f8cd3757104a7cdae0a1eafc5af11dc4a87830c62b02cbb8c82019a"
 
   def install
     mv('dyff-darwin-amd64', 'dyff')
--- HomebrewFormula/gonut.rb
+++ HomebrewFormula/gonut.rb
@@ -1,40 +1,12 @@
-# MIT License
-#
-# Copyright (c) 2019 The Homeport Team
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-require 'FileUtils'
-
-# Just the gonut homebrew formula
 class Gonut < Formula
-  yver = "v0.14.0"
-  ysha = "97c5040923bd0fceed13f692ca844952b9e18343ccfe64f17fd982671e16f546"
-
-  desc 'gonut - a portable tool to help you verify whether you can push a sample app to a Cloud Foundry'
-  homepage 'https://github.com/homeport/gonut'
-  url "https://github.com/homeport/gonut/releases/download/#{yver}/gonut-darwin-amd64"
-  sha256 "#{ysha}"
-  version "#{yver}"
+  desc "gonut - a portable tool to help you verify whether you can push a sample app to a Cloud Foundry"
+  homepage "https://github.com/homeport/gonut"
+  url "https://github.com/homeport/gonut/releases/download/v0.14.0/gonut-darwin-amd64"
+  version "v0.14.0"
+  sha256 "97c5040923bd0fceed13f692ca844952b9e18343ccfe64f17fd982671e16f546"
 
   def install
-    FileUtils.mv('gonut-darwin-amd64', 'gonut')
+    mv('gonut-darwin-amd64', 'gonut')
     bin.install 'gonut'
   end
 
