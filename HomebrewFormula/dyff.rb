a940d28db79a58a1955f516f8da7d176cb184e79
Matthias Diester <mddiester@gmail.com>
2019-07-17 10:21:10 +0200

    Inline shasum and version
    Inline the shasum and version variable and place them in the respective strings.

--- HomebrewFormula/dyff.rb
+++ HomebrewFormula/dyff.rb
@@ -2,14 +2,11 @@ require 'FileUtils'
 
 # Just the dyff homebrew formula
 class Dyff < Formula
-  dver = "v0.10.0"
-  dsha = "0d41fd8f7857dd12eb02722aafbbfe9d594cf6bb156b6e0edcece21e210dfc15"
-
   desc 'δyƒƒ /ˈdʏf/ - A diff tool for YAML files, and sometimes JSON'
   homepage 'https://github.com/homeport/dyff'
-  url "https://github.com/homeport/dyff/releases/download/#{dver}/dyff-darwin-amd64"
-  sha256 "#{dsha}"
-  version "#{dver}"
+  url "https://github.com/homeport/dyff/releases/download/v0.10.0/dyff-darwin-amd64"
+  sha256 "0d41fd8f7857dd12eb02722aafbbfe9d594cf6bb156b6e0edcece21e210dfc15"
+  version "v0.10.0"
 
   def install
     FileUtils.mv('dyff-darwin-amd64', 'dyff')
