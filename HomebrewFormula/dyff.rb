9fa0a86bb0cbe52210a7b9def1c871240e65b726
Matthias Diester <mddiester@gmail.com>
2019-07-17 10:27:49 +0200

    Use single quotes
    Use single quotes to have the same style in the formula.

--- HomebrewFormula/dyff.rb
+++ HomebrewFormula/dyff.rb
@@ -4,9 +4,9 @@ require 'FileUtils'
 class Dyff < Formula
   desc 'δyƒƒ /ˈdʏf/ - A diff tool for YAML files, and sometimes JSON'
   homepage 'https://github.com/homeport/dyff'
-  url "https://github.com/homeport/dyff/releases/download/v0.10.0/dyff-darwin-amd64"
-  sha256 "0d41fd8f7857dd12eb02722aafbbfe9d594cf6bb156b6e0edcece21e210dfc15"
-  version "v0.10.0"
+  url 'https://github.com/homeport/dyff/releases/download/v0.10.0/dyff-darwin-amd64'
+  sha256 '0d41fd8f7857dd12eb02722aafbbfe9d594cf6bb156b6e0edcece21e210dfc15'
+  version 'v0.10.0'
 
   def install
     FileUtils.mv('dyff-darwin-amd64', 'dyff')
