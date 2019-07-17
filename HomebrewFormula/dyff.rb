2f9768da066a7db04a12a92162533eac69a2d58d
Matthias Diester <mddiester@gmail.com>
2019-07-17 10:35:48 +0200

    Remove version from dyff formula
    Let `brew` parse the version from the URL.

--- HomebrewFormula/dyff.rb
+++ HomebrewFormula/dyff.rb
@@ -6,7 +6,6 @@ class Dyff < Formula
   homepage 'https://github.com/homeport/dyff'
   url 'https://github.com/homeport/dyff/releases/download/v0.10.0/dyff-darwin-amd64'
   sha256 '0d41fd8f7857dd12eb02722aafbbfe9d594cf6bb156b6e0edcece21e210dfc15'
-  version 'v0.10.0'
 
   def install
     FileUtils.mv('dyff-darwin-amd64', 'dyff')
