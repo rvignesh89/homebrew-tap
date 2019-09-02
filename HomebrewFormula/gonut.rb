fc2054c26dec6775d7697b2744e78230c6661c20
Matthias Diester <matthias.diester@de.ibm.com>
2019-09-02 21:14:53 +0200

    gonut v2.0.0

--- HomebrewFormula/gonut.rb
+++ HomebrewFormula/gonut.rb
@@ -1,9 +1,9 @@
 class Gonut < Formula
   desc "gonut - a portable tool to help you verify whether you can push a sample app to a Cloud Foundry"
   homepage "https://github.com/homeport/gonut"
-  url "https://github.com/homeport/gonut/releases/download/v0.14.0/gonut-darwin-amd64"
-  version "v0.14.0"
-  sha256 "97c5040923bd0fceed13f692ca844952b9e18343ccfe64f17fd982671e16f546"
+  url "https://github.com/homeport/gonut/releases/download/v2.0.0/gonut-darwin-amd64"
+  version "v2.0.0"
+  sha256 "70b861b4a7403c91175e5361b39ff480dc72ddab4d230517395cad612159ebb4"
 
   def install
     mv('gonut-darwin-amd64', 'gonut')
