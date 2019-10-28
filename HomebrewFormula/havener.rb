6ed7ad76ce6b096e83d9872f51037533683a36b3
Matthias Diester <matthias.diester@de.ibm.com>
2019-10-28 13:55:09 +0100

    havener v1.4.2

--- HomebrewFormula/havener.rb
+++ HomebrewFormula/havener.rb
@@ -1,9 +1,9 @@
 class Havener < Formula
   desc "Havener - Convenience tool to handle tasks around Containerized CF workloads on a Kubernetes cluster"
   homepage "https://github.com/homeport/havener"
-  url "https://github.com/homeport/havener/releases/download/v1.4.1/havener-darwin-amd64"
-  version "v1.4.1"
-  sha256 "8322cd4c0eb0c38567493a9e8c9cb577b8787dda038dea0f74502fc935e1fac3"
+  url "https://github.com/homeport/havener/releases/download/v1.4.2/havener-darwin-amd64"
+  version "v1.4.2"
+  sha256 "9c5154e7be31ca5c5eb42a00ed8cad2ed90f3e36d82f07efbb5b384ef2e28c6e"
 
   def install
     mv('havener-darwin-amd64', 'havener')
