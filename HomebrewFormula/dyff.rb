c923ec8e3776a011cf0eb02998a51a5fd75221d2
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-04 15:42:04 +0100

    dyff v1.1.3

--- HomebrewFormula/dyff.rb
+++ HomebrewFormula/dyff.rb
@@ -1,9 +1,9 @@
 class Dyff < Formula
   desc "δyƒƒ /ˈdʏf/ - A diff tool for YAML files, and sometimes JSON"
   homepage "https://github.com/homeport/dyff"
-  url "https://github.com/homeport/dyff/releases/download/v1.1.2/dyff-darwin-amd64"
-  version "v1.1.2"
-  sha256 "9fe2897bce47888788416771c9de4911a3289d4242af1d0afc1cae3728ea6bc2"
+  url "https://github.com/homeport/dyff/releases/download/v1.1.3/dyff-darwin-amd64"
+  version "v1.1.3"
+  sha256 "5aae7370da6a62a10dc3efa10adc3094a201996f919eefe41d6e015696c00585"
 
   def install
     mv('dyff-darwin-amd64', 'dyff')
