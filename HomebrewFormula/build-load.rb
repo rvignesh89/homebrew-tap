3b5e0a11df7dd4430147c99e0cab135d63bcbd41
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-09 21:30:54 +0100

    build-load v0.3.4

--- HomebrewFormula/build-load.rb
+++ HomebrewFormula/build-load.rb
@@ -1,9 +1,9 @@
 class BuildLoad < Formula
   desc "build-load - command line tool to create synthetic load for https://github.com/shipwright-io/build"
   homepage "https://github.com/homeport/build-load"
-  url "https://github.com/homeport/build-load/releases/download/v0.3.3/build-load-darwin-amd64"
-  version "v0.3.3"
-  sha256 "25d288fcec152e3a5654cd484188ac6d6344b970ceb508439c5db7e3f533e0db"
+  url "https://github.com/homeport/build-load/releases/download/v0.3.4/build-load-darwin-amd64"
+  version "v0.3.4"
+  sha256 "d5b4eaf9310603268b1a757472a407ac9d7f133edf03a1b6f32c20179facafa2"
 
   def install
     mv('build-load-darwin-amd64', 'build-load')
