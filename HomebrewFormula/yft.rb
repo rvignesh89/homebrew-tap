c24e4bc679c9b72fe543f62ff5843dd3813566df
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-16 14:02:39 +0100

    yft v1.0.1

--- HomebrewFormula/yft.rb
+++ HomebrewFormula/yft.rb
@@ -1,9 +1,9 @@
 class Yft < Formula
   desc "yft - YAML file tool"
   homepage "https://github.com/homeport/yft"
-  url "https://github.com/homeport/yft/releases/download/v1.0.0/yft-darwin-amd64"
-  version "v1.0.0"
-  sha256 "78d3fd864aac8a018668fdd10b75522ff966cc181602bce60ae9164a8fbd3865"
+  url "https://github.com/homeport/yft/releases/download/v1.0.1/yft-darwin-amd64"
+  version "v1.0.1"
+  sha256 "05cdaeea97ea99ef88ade62d0ca31921f122a92bf9c3dc05b7e152859e5d24d7"
 
   def install
     mv('yft-darwin-amd64', 'yft')
