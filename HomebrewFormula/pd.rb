a62a5580b08051790dbd7f80b3117a2cb41edb3a
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-18 22:15:19 +0100

    pd v2.5.0

--- HomebrewFormula/pd.rb
+++ HomebrewFormula/pd.rb
@@ -1,9 +1,9 @@
 class Pd < Formula
   desc "pd - command line tool to query PagerDuty for the current on-calls"
   homepage "https://github.com/homeport/pd"
-  url "https://github.com/homeport/pd/releases/download/v2.4.0/pd-darwin-amd64"
-  version "v2.4.0"
-  sha256 "a465881c86433bdb34da442e2a0a00bfd860f9827f70ba7b63a04a2f7ac73dc2"
+  url "https://github.com/homeport/pd/releases/download/v2.5.0/pd-darwin-amd64"
+  version "v2.5.0"
+  sha256 "fa21f9412a62bf2cf747c63df618e0072dc74fe78c8c3b811239959180c181b3"
 
   def install
     mv('pd-darwin-amd64', 'pd')
